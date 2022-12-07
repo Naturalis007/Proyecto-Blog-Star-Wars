69381fb95c5df8b64ec26b75ea8225f13189481a
Naturalis007 <tamara.salasvergara@gmail.com>
2022-12-07 13:47:27 +0000

    en el navbar agregué botón volver a pag principal, modificqué color y emoji corazon

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-secondary mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<button className="btn btn-warning m-3">Página principal</button>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-warning m-3">Mis Favs 🖤</button>
 				</Link>
 			</div>
 		</nav>
